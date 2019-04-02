2c97c6be16e21f4e818876ee885867a95f426a1b
Bob Brown <bob@bigleafdesigns.com>
2019-04-02 15:28:08 -0600

    Cleanup

--- test/UnitTests/src/BBCMacros_Test.cpp
+++ test/UnitTests/src/BBCMacros_Test.cpp
@@ -1,15 +1,1 @@
-//  Copyright (c) 2019 Bob Brown. All rights reserved.
-//
-
-#pragma once
-
-#define STRINGIFY(s) XSTRINGIFY(s)
-#define XSTRINGIFY(s) #s
-
-#define BBC_MACRO_BLOCK(x) do {x} while (false)
-
-#if DEBUG
-#ifndef BBC_DEBUG
-#define BBC_DEBUG
-#endif
 
