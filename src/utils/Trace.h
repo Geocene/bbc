d6d0eca5229d13eabe9ff2ff02914f58056fc586
Bob Brown <bob@bigleafdesigns.com>
2019-04-03 19:41:21 -0600

    Implementing BBCTrace

--- src/utils/Trace.h
+++ src/utils/Trace.h
@@ -3,24 +3,272 @@
 
 #pragma once
 
+#include <regex>
+#include <fstream>
+
 #include "BBCAssert.h"
+#include "BBCMacros.h"
 #include "Singleton.h"
+
+#ifdef BBC_DEBUG
+#define BBC_TRACE(mask, ...) BBC_MACRO_BLOCK(Trace::instance().writeTrace(mask, __VA_ARGS__);)
+#else
+#define BBC_TRACE(...)
+#endif
+
+/*
+ WIP...
+ 
+#define BBC_TRACE_LINE_INFO __FILE__, STRINGIFY(: __LINE__)
+
+#ifdef BBC_DEBUG
+#define BBC_TRACE_LINE(mask, ...) BBC_MACRO_BLOCK(Trace::instance().writeTrace(mask, __FILE__, __VA_ARGS__);)
+#else
+#define BBC_TRACE_LINE(...)
+#endif
+*/
+
+#define BBC_TRACE_R(mask, ...) BBC_MACRO_BLOCK(Trace::instance().writeTrace(mask, __VA_ARGS__);)
+
+#define BBC_BOOL_TO_STRING(x) x ? "true" : "false"
+
+// One of the many clever ways to print 64-bit values in hex
+// 0x%jx or 0x%jX or %#018lllx or %#018lllX
+//
 
 class Trace : public Singleton<Trace>
 {
 public:
     
-    bool initialize(const std::string& iTraceConfigFile)
+    typedef uint64_t TraceMask;
+    typedef void (*TraceCallback)(const char* iMessage);
+
+    enum Priority : uint64_t
+    {
+          kPriority_Off     = 0x0000000000000000
+
+        , kPriority_Low     = 0x1000000000000000
+        , kPriority_Medium  = 0x2000000000000000
+        , kPriority_High    = 0x3000000000000000
+
+        , kPriority_Always  = 0x4000000000000000
+    };
+
+    enum Category : uint64_t
+    {
+          kCategory_None                = 0x0000000000000000
+        
+        , kCategory_Basic               = 0x0000000000000001
+        
+        , kCategory_All                 = 0x0FFFFFFFFFFFFFFF
+    };
+
+#define PRIORITY_TO_STRING(iVal) \
+if (iPriority == iVal) \
+    return STRINGIFY(iVal);
+    
+    static std::string priorityAsString(Priority iPriority)
+    {
+        PRIORITY_TO_STRING(kPriority_Off);
+        PRIORITY_TO_STRING(kPriority_Low);
+        PRIORITY_TO_STRING(kPriority_Medium);
+        PRIORITY_TO_STRING(kPriority_High);
+        PRIORITY_TO_STRING(kPriority_Always);
+
+        BBC_ASSERT(!"priorityAsString - unknown iPriority!");
+
+        // Satisfy the return value
+        //
+        return "";
+    }
+
+#define STRING_TO_PRIORITY(iVal) \
+if (0 == strcmp(iStr.c_str(), STRINGIFY(iVal))) \
+    return iVal;
+
+    static Priority stringToPriority(const std::string& iStr)
+    {
+        STRING_TO_PRIORITY(kPriority_Off);
+        STRING_TO_PRIORITY(kPriority_Low);
+        STRING_TO_PRIORITY(kPriority_Medium);
+        STRING_TO_PRIORITY(kPriority_High);
+        STRING_TO_PRIORITY(kPriority_Always);
+
+        BBC_ASSERT(!"stringToPriority - unknown iStr!");
+        
+        // Satisfy the return value
+        //
+        return kPriority_Off;
+    }
+
+#define CATEGORY_TO_STRING(iVal) \
+if (iCategory == iVal) \
+    return STRINGIFY(iVal);
+    
+    static std::string categoryAsString(Category iCategory)
+    {
+        CATEGORY_TO_STRING(kCategory_None);
+        CATEGORY_TO_STRING(kCategory_Basic);
+        CATEGORY_TO_STRING(kCategory_All);
+        
+        BBC_ASSERT_R(!"categoryAsString - unknown iCategory!");
+        
+        // Satisfy the return value
+        //
+        return "";
+    }
+    
+#define STRING_TO_CATEGORY(iVal) \
+if (0 == strcmp(iStr.c_str(), STRINGIFY(iVal))) \
+    return iVal;
+    
+    static Category stringToCategory(const std::string& iStr)
+    {
+        STRING_TO_CATEGORY(kCategory_None);
+        STRING_TO_CATEGORY(kCategory_Basic);
+        STRING_TO_CATEGORY(kCategory_All);
+        
+        BBC_ASSERT_R(!"stringToCategory - unknown iStr!");
+        
+        // Satisfy the return value
+        //
+        return kCategory_None;
+    }
+
+    bool initializeWithBuffer(const std::string& iTraceConfig, TraceCallback iCallback = nullptr, bool iUseBoost = true)
     {
         if (initalized_)
             return true;
         
+        processConfig(iTraceConfig);
+        
+        callback_ = iCallback;
         initalized_ = true;
         
         return true;
     }
+
+    bool initializeWithFile(const std::string& iTraceConfigFile, TraceCallback iCallback = nullptr, bool iUseBoost = true)
+    {
+        if (initalized_)
+            return true;
+        
+        std::ifstream fileStream;
+        fileStream.open(iTraceConfigFile);
+        
+        // See if the file exists
+        //
+        if (!fileStream.is_open())
+            return false;
+
+        std::stringstream buffer;
+        buffer << fileStream.rdbuf();
+        processConfig(buffer.str());
+        
+        fileStream.close();
+        
+        callback_ = iCallback;
+        initalized_ = true;
+        
+        return true;
+    }
+
+    void reset()
+    {
+        initalized_ = false;
+        
+        traceAll_ = false;
+        masks_.clear();
+        
+        callback_ = nullptr;
+    }
+
+    void writeTrace(TraceMask iMask, const char* iArgs...) const
+    {
+        bool doTrace = false;
+        
+        if (iMask)
+            doTrace = true;
+        
+        if (doTrace)
+        {
+            va_list argList;
+            va_start(argList, iArgs);
+            
+            // Buffer to print the message to,
+            // must include terminiating character
+            //
+            char traceMessage[sTraceMessageSize];
+            memset(traceMessage, 0, sTraceMessageSize);
+            
+            vsnprintf(traceMessage, sTraceMessageSize, iArgs, argList);
+            
+            va_end(argList);
+
+
+            if (callback_)
+            {
+                callback_(traceMessage);
+            }
+            else
+            {                
+                std::cout << traceMessage << std::endl;
+            }
+        }
+    }
     
 private:
     
+    void processConfig(const std::string& iTraceConfig)
+    {
+        std::stringstream ss(iTraceConfig);
+        std::string line;
+        while (std::getline(ss, line))
+        {
+            // Trim leading and trailing spaces
+            // Fancy regex trimming
+            //
+            line = std::regex_replace(line, std::regex("^ +| +$|( ) +"), "$1");
+
+            // Check for # indicating a commented out value
+            //
+            if (line.length() && line[0] == '#')
+                continue;
+            
+            // Split at the @
+            //
+            std::string categoryStr = line.substr(0, line.find("@"));
+            std::string priorityStr = line.substr(line.find("@") + 1, line.length());
+
+            // Trim leading and trailing spaces
+            //
+            categoryStr = std::regex_replace(categoryStr, std::regex("^ +| +$|( ) +"), "$1");
+            priorityStr = std::regex_replace(priorityStr, std::regex("^ +| +$|( ) +"), "$1");
+
+            Category category = stringToCategory(categoryStr);
+            Priority priority = stringToPriority(priorityStr);
+            
+            TraceMask mask = category | priority;
+            
+            // Filter duplicates
+            //
+            if (std::find (masks_.begin(), masks_.end(), mask) != masks_.end())
+                continue;
+            
+            // Finally add the entry to the list
+            //
+            masks_.push_back(mask);
+            
+            std::cout << line << std::endl;
+        }
+    }
+    
+    static const int32_t sTraceMessageSize{2048};
+    
     bool initalized_{false};
+    
+    bool traceAll_{false};
+    std::vector<TraceMask> masks_;
+    
+    TraceCallback callback_{nullptr};
 };
--- test/UnitTests/src/Trace_Test.cpp
+++ test/UnitTests/src/Trace_Test.cpp
@@ -4,8 +4,77 @@
 #include "gtest/gtest.h"
 #include "Trace.h"
 
+static void TestTraceCallback(const char* iMessage)
+{
+    std::cout << iMessage << std::endl;
+}
+
+TEST(TraceTest, TraceTest_Test4)
+{
+    Trace::instance().initializeWithFile("BBCTrace.config");
+    Trace::instance().reset();
+}
+
 TEST(TraceTest, TraceTest_Test1)
 {
-    Trace::instance().initialize("");
+    Trace::instance().initializeWithBuffer("    kCategory_Basic@kPriority_Low \n   #kCategory_Basic@kPriority_High\nkCategory_Basic@kPriority_Medium", TestTraceCallback);
+    
+    std::string worldStr = "world";
+
+    BBC_TRACE(Trace::kPriority_Always | Trace::kCategory_All, "Hello world!");
+    BBC_TRACE_R(Trace::kPriority_Always | Trace::kCategory_All, "Hello world!");
+
+    BBC_TRACE(Trace::kPriority_Always | Trace::kCategory_All, "Hello %s!", worldStr.c_str());
+    BBC_TRACE_R(Trace::kPriority_Always | Trace::kCategory_All, "Hello world!");
+
+    BBC_TRACE(Trace::kPriority_Always | Trace::kCategory_All, "Hello %s - %d %f!"
+              , worldStr.c_str()
+              , 123
+              , 3.14
+              );
+    BBC_TRACE_R(Trace::kPriority_Always | Trace::kCategory_All, "Hello %s - %d %f!"
+              , worldStr.c_str()
+              , 123
+              , 3.14
+              );
+
+    int64_t test64Hex = 0xFFFFFFFFFFFFFFFF;
+    BBC_TRACE(Trace::kPriority_Always | Trace::kCategory_All, "0x%jX"
+              , test64Hex
+              );
+
+    BBC_TRACE(Trace::kPriority_Always | Trace::kCategory_All, "%s != %s"
+              , BBC_BOOL_TO_STRING(true)
+              , BBC_BOOL_TO_STRING(false)
+              );
+
+    Trace::instance().reset();
+}
+
+
+TEST(TraceTest, TraceTest_Test2)
+{
+    EXPECT_EQ(Trace::priorityAsString(Trace::kPriority_Off), "kPriority_Off");
+    EXPECT_EQ(Trace::priorityAsString(Trace::kPriority_Low), "kPriority_Low");
+    EXPECT_EQ(Trace::priorityAsString(Trace::kPriority_Medium), "kPriority_Medium");
+    EXPECT_EQ(Trace::priorityAsString(Trace::kPriority_High), "kPriority_High");
+    EXPECT_EQ(Trace::priorityAsString(Trace::kPriority_Always), "kPriority_Always");
+
+    EXPECT_EQ(Trace::stringToPriority("kPriority_Off"), Trace::kPriority_Off);
+    EXPECT_EQ(Trace::stringToPriority("kPriority_Low"), Trace::kPriority_Low);
+    EXPECT_EQ(Trace::stringToPriority("kPriority_Medium"), Trace::kPriority_Medium);
+    EXPECT_EQ(Trace::stringToPriority("kPriority_High"), Trace::kPriority_High);
+    EXPECT_EQ(Trace::stringToPriority("kPriority_Always"), Trace::kPriority_Always);
+}
+
+TEST(TraceTest, TraceTest_Test3)
+{
+    EXPECT_EQ(Trace::categoryAsString(Trace::kCategory_None), "kCategory_None");
+    EXPECT_EQ(Trace::categoryAsString(Trace::kCategory_Basic), "kCategory_Basic");
+    EXPECT_EQ(Trace::categoryAsString(Trace::kCategory_All), "kCategory_All");
+    
+    EXPECT_EQ(Trace::stringToCategory("kCategory_None"), Trace::kCategory_None);
+    EXPECT_EQ(Trace::stringToCategory("kCategory_Basic"), Trace::kCategory_Basic);
+    EXPECT_EQ(Trace::stringToCategory("kCategory_All"), Trace::kCategory_All);
 }
 
