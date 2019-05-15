7669dbb5680c54db2e14a7174c07c26c10c2a2df
Bob Brown <bob@bigleafdesigns.com>
2019-05-15 16:18:37 -0600

    Adding Doxygen to Singleton

--- src/utils/Singleton.h
+++ src/utils/Singleton.h
@@ -3,10 +3,32 @@
 
 #pragma once
 
+/**
+ * \brief Templated Singleton class
+ *
+ * Note - There is not destructor as this object exists
+ *        the entire time of the program.
+ *        All resources will be released when the program is terminated.
+ *        Any implementation of a Singleton should not hold resources
+ *        such as open files, ports, or other items that would be
+ *        cleaned up at destruction time.
+ */
 template <class _class_>
 class Singleton
 {
 public:
+    
+    /**
+     * Returns a reference to the singleton.
+     * This is a static function.
+     *
+     * Example use case:
+     *
+     *      class SingletonTest1 : public Singleton<SingletonTest1> {};
+     *      SingletonTest1::instance();
+     *
+     * @return Returns a reference to the singleton object
+     */
     static _class_& instance()
     {
         if (instance_ == nullptr)
@@ -15,12 +37,15 @@ public:
         }
         
         return *instance_;
-        //return *instance_.get();
     }
     
 private:
+    
+    /// The Singleton pointer for the class.
     static std::unique_ptr<_class_> instance_;
 };
 
+/// Defining the singleton static instance and setting it to nullptr
+/// This is done in the header as it is a templated type.
 template <class _class_>
 std::unique_ptr<_class_> Singleton<_class_>::instance_ = nullptr;
